7d5681905f7f557a441956d15678e4eb88ce002f
mdfaizan973 <faizan.md9735@gmail.com>
2024-08-10 17:22:28 +0530

    added link sectin

---
+++ faizanportfolio2/src/App.jsx
@@ -0,0 +1,61 @@
+import Sidebar from "./PortfolioComponent2/Sidebar";
+import MiddleContaner from "./PortfolioComponent2/MiddleContaner";
+import LinkContainer from "./PortfolioComponent2/LinkContainer";
+import { styled } from "styled-components";
+import "./App.css";
+
+export default function App() {
+  return (
+    <MainPortfolioTwoContainer>
+      <div className="sidebarcontainer">
+        <Sidebar />
+      </div>
+      <div className="maincontainer">
+        <MiddleContaner />
+      </div>
+      <div className="linkcontainer">
+        <LinkContainer />
+      </div>
+    </MainPortfolioTwoContainer>
+  );
+}
+
+const MainPortfolioTwoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  width: 90%;
+  margin: 100px auto;
+  font-family: -apple-system, BlinkMacSystemFont, "Roboto", sans-serif !important;
+  /* font-family: cursive; */
+
+  .sidebarcontainer {
+    width: 20%;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+    height: 35rem;
+    position: fixed;
+    left: 5%;
+    background-color: white;
+    border-radius: 20px;
+  }
+
+  .maincontainer {
+    width: 60%;
+    height: 900px;
+    /* box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px; */
+    margin-left: 27%;
+    overflow-y: auto;
+    background-color: white;
+    border-radius: 20px;
+  }
+
+  .linkcontainer {
+    width: 8%;
+    /* box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px; */
+    height: 35rem;
+    position: fixed;
+    right: 5%;
+    background-color: white;
+    border-radius: 20px;
+  }
+`;
---
+++ faizanportfolio2/src/PortfolioComponent2/MiddleContaner.jsx
@@ -0,0 +1,3 @@
+export default function MiddleContaner() {
+  return <div>MiddleContaner</div>;
+}
